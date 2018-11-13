e15e57672b520e3c5e32a0dae30f171015145b73
leticia <leticia.nhundu@dariel.co.za>
2018-11-13 07:27:06 +0200

    Add home page

--- Bright/src/app/app.module.ts
+++ Bright/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule , Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
